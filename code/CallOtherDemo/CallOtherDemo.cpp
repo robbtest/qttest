2ba5e1f4705110c79096fade377a27b7633949ef
robb <364886923@qq.com>
2019-01-12 21:48:31 +0800

    增加线程控制代码

--- code/CallOtherDemo/CallOtherDemo.cpp
+++ code/CallOtherDemo/CallOtherDemo.cpp
@@ -1,15 +1,31 @@
 #include "CallOtherDemo.h"
 
 #include <qt_windows.h>
+#include <QDebug>
+#include <QTime>
+#include <QThread>
+#include <QScrollBar>
 
 CallOtherDemo::CallOtherDemo(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
 
+	ui.ThreadControlWid->setCheckable(true);
+	QString strText = !ui.ThreadControlWid->isChecked() ? "Start" : "Stop";
+	ui.ThreadControlWid->setText(strText);
+
 	ui.CallDemoWid->installEventFilter(this);
+
+	connect(ui.ThreadControlWid, &QPushButton::clicked, this, &CallOtherDemo::slotThread);
+	connect(this, &CallOtherDemo::sigText, this, &CallOtherDemo::slotText);
 }
-#include <tchar.h>
+
+CallOtherDemo::~CallOtherDemo()
+{
+	StopThread();
+}
+
 bool CallOtherDemo::eventFilter(QObject* pObj, QEvent* pEvent)
 {
 	if (pObj == ui.CallDemoWid)
@@ -24,4 +40,73 @@ bool CallOtherDemo::eventFilter(QObject* pObj, QEvent* pEvent)
 		}
 	}
 	return QWidget::eventFilter(pObj, pEvent);
+}
+
+void CallOtherDemo::slotThread(bool bStart)
+{
+	ui.ThreadControlWid->setEnabled(false);
+
+	bStart ? StartThread() : StopThread();
+	if (bStart)
+	{
+		ui.ThreadTextWid->setPlainText("");
+	}
+
+	QString strText = !bStart ? "Start" : "Stop";
+	ui.ThreadControlWid->setText(strText);
+	ui.ThreadControlWid->setEnabled(true);
+}
+
+void CallOtherDemo::slotText(QString strText)
+{
+	ui.ThreadTextWid->setPlainText(ui.ThreadTextWid->toPlainText() + strText + "\n");
+
+	QScrollBar *scrollbar = ui.ThreadTextWid->verticalScrollBar();
+	if (scrollbar)
+	{
+		scrollbar->setSliderPosition(scrollbar->maximum());
+	}
+}
+
+/*
+	@说明 joinable() -> 查看线程是否活跃，即是否在执行函数体
+	      join()     -> 等待线程执行完成和退出，即阻塞在此处
+		  m_bExit    -> 控制线程中循环体是否退出标志 true表示退出循环体
+*/
+void CallOtherDemo::StartThread()
+{
+	if (m_bExit)
+	{
+		m_bExit = false;
+		if (m_thread.joinable())
+		{
+			m_thread.join();
+		}
+		m_thread = std::thread(&CallOtherDemo::ThreadFuncProc, this);
+	}
+}
+
+void CallOtherDemo::StopThread()
+{
+	if (!m_bExit)
+	{
+		m_bExit = true;
+		m_thread.join();
+	}
+}
+
+void CallOtherDemo::ThreadFuncProc()
+{
+	int iCount = 0;
+
+	while (!m_bExit)
+	{
+		iCount++;
+		QString str = QString("The %1th time: %2").arg(iCount, 3).arg(QTime::currentTime().toString("HH:mm:ss"));
+		QThread::msleep(500);
+		
+		emit sigText(str);
+	}
+
+	m_bExit = true;
 }
--- code/CallOtherDemo/CallOtherDemo.h
+++ code/CallOtherDemo/CallOtherDemo.h
@@ -3,16 +3,34 @@
 #include <QtWidgets/QWidget>
 #include "ui_CallOtherDemo.h"
 
+#include <thread>
+
 class CallOtherDemo : public QWidget
 {
 	Q_OBJECT
 
 public:
 	CallOtherDemo(QWidget *parent = Q_NULLPTR);
+	~CallOtherDemo();
+
+Q_SIGNALS:
+	void sigText(QString);
 
 protected:
-	bool CallOtherDemo::eventFilter(QObject* pObj, QEvent* pEvent);
+	// 事件过滤器
+	bool eventFilter(QObject* pObj, QEvent* pEvent);
+	// 槽函数
+	void slotThread(bool bStart);
+	void slotText(QString strText);
+	// 启动和停止线程
+	void StartThread();
+	void StopThread();
+
+	// 线程执行体
+	void ThreadFuncProc();
 
 private:
-	Ui::CallOtherDemoClass ui;
+	Ui::CallOtherDemoClass  ui;					// 界面
+	bool                    m_bExit = true;		// 控制线程退出标志
+	std::thread				m_thread;			// 标准线程对象
 };
